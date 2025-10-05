7fe9382264974d2d9d61b24021352082718db855
Esther Manrique <esthermanriqueepam@gmail.com>
2025-10-04 19:42:50 -0600

    feat: enhance search functionality and UI for GIFs, including search history integration

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,9 +1,23 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
+import { map, tap } from 'rxjs';
+
+// Ejemplo del objeto que se armará para la busqueda.
+// {
+//   'goku': [gif1, gif2, gif3],
+//   'saitama': [gif1, gif2, gif3],
+//   'dragon ball': [gif1, gif2, gif3],
+// }
+
+// Esto es el tipado de la señal
+// Record<string, Gif[]> Es lo de la linea 27
+
+// Asi es como se define la señal para el objeto de historial de busquedas
+// searchHistory = signal<Record<string, Gif[]>>({});
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
@@ -12,12 +26,16 @@ export class GifsService {
   trendingGifs = signal<Gif[]>([]);
   trendingGifsLoading = signal<boolean>(true);
 
+  searchHistory = signal<Record<string, Gif[]>>({});
+  searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
+
   constructor() {
     this.loadTrendingGifs();
+    this.searchGifs('matrix');
     console.log('Servicio Creado!!!');
   }
 
-  loadTrendingGifs(): void {
+  loadTrendingGifs() {
     this.http
       .get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
         params: {
@@ -29,8 +47,36 @@ export class GifsService {
         const gifs = GifMapper.mapGiphyItemsToGifArray(response.data);
         this.trendingGifs.set(gifs);
         this.trendingGifsLoading.set(false);
-        console.log(gifs);
-        
+        console.log({ gifs });
       });
+  }
+
+  searchGifs(query: string) {
+    return this.http
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+        params: {
+          api_key: environment.giphyApiKey,
+          q: query,
+          limit: '20',
+        },
+      })
+      .pipe(
+        map(({ data }) => data),
+        map((items) => GifMapper.mapGiphyItemsToGifArray(items)),
+
+        // TODO: Historial de busquedas
+        tap((items) => {
+          this.searchHistory.update((history) => ({
+            ...history,
+            [query.toLowerCase()]: items,
+          }));
+        })
+      );
+
+    // .subscribe((response) => {
+    //   const gifs = GifMapper.mapGiphyItemsToGifArray(response.data);
+
+    //   console.log({ shearch: gifs });
+    // });
   }
 }
